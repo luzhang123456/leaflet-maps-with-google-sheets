80204bc40a1e0b180ee35c4cf4016fe8e61fda18
luzhang123456 <33830204+luzhang123456@users.noreply.github.com>
2018-02-24 10:40:43 +0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10MCKpTTXS1K_Cny6v1Lxpn2fIf35-USd38yeTHD9gZU/edit#gid=0';
